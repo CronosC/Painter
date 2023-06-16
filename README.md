22a55bb41badb6f4b0edccc3d23db29e1f5e42fd
cd <Chr.Do93@gmail.com>
2023-06-16 07:24:37 +0200

    current state

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # stableDiffusionTesting
+
+Setup:
+    - setup the env with conda
+    - download a model (e.g. from civitai)
+    - put into model folder, adjust path in run.py
+
+Running:
+    - fill out prompt with one or more prompts. Each line is interpreted as one prompt. For each line one result .png is created.
+    - fill HQ file with modifiers which are used for all prompts. Should only have one line. Can not be empty, but may be only a " "
+    - fill negative file with one or more negative prompts. These inform the process on what to avoid. Can have multiple lines, each line is interpreted as one negative prompt. Each combination of prompt x negative prompt is created as one result file.
+
+    - OPTIONAL: Fill out X_axis/Y_axis files. Three lines would make each result .png a 3x3 grid of individual pictures, where the different lines in x_axis are appended to each prompt and vary from left to right. (Same idea for y_axis)
+    - The different versions along an axis share the same seed! The only difference is the difference in lines in the axis files.
+    - The call to paint_axis_from_file needs to take the file name ("X_axis", "Y_axis") as 3rd/4th argument to work.
+    - The other option is to simply input a number as 3rd/4th argument, which does not vary the pictures in the grid, but instead just creates multiple versions with different seeds.
+    - bigger grids need more gpu memory
+
+    - e.g: red, green, blue as lines in x_axis, 1 as 4th argument creates a grid like this:
+    | picture in red | picture in green | picture in blue |
+    - if we add an y axis with two entries e.g. watercolor and digital art the result is:
+    | watercolor in red | watercolor in green | watercolor in blue |
+    ----------------------------------------------------------------
+    | digitalart in red | digitalart in green | digitalart in blue |
--- run.py
+++ run.py
@@ -2,9 +2,18 @@ import torch
 from diffusers import StableDiffusionPipeline
 from diffusers import StableDiffusionInstructPix2PixPipeline
 from diffusers import DPMSolverMultistepScheduler
+from diffusers import StableDiffusionUpscalePipeline
+from diffusers import StableDiffusionImg2ImgPipeline
+from transformers import CLIPImageProcessor
+from transformers import CLIPTokenizer
 import PIL
 import requests
 import os
+from compel import Compel
+import random
+import math
+from io import BytesIO
+
 
 torch.cuda.empty_cache()
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:64"
@@ -29,73 +38,182 @@ def download_image(url):
    image = image.convert("RGB")
    return image
 
-dif_prompts = [
-"alien landscape, weird, strange, plants, ruins",
-"lonely house, old, delapidated",
-"selfie, woman, bedroom",
-"group of people at festival, music, dance",
-"large machine, inconceivable, unknown",
-"aerial shot of woods, zoomed out",
-"hotel room, luxurious",
-"mountainous landscape, snow, clouds",
-"robot, portrait, sci-fi",
-"banker, suit",
-"mass of wriggling worms"
-]
+class Painter:
+    # guidance_scale: adherence to prompt ~7-8.5 generally good
+    # num_inference_steps: ~quality, 50 is normal, less is faster
+    def __init__(self, modelT2I):
+        if modelT2I == 1:
+            self.pipeT2I = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=torch.float16)
+            self.pipeI2I = StableDiffusionImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=torch.float16)
+        elif modelT2I == 2:
+            self.pipeT2I = StableDiffusionPipeline.from_ckpt("models/dreamshaper_6BakedVae.safetensors", torch_dtype=torch.float16)
+            self.pipeI2I = StableDiffusionImg2ImgPipeline.from_ckpt("models/dreamshaper_6BakedVae.safetensors", torch_dtype=torch.float16)
+            #self.pipeT2I.load_textual_inversion("embeddings/BadDream.pt")
+            #self.pipeT2I.load_textual_inversion("embeddings/UnrealisticDream.pt")
+            self.pipeT2I.load_textual_inversion("embeddings/FastNegativeEmbedding.pt")
+        elif modelT2I == 3:
+            self.pipeT2I = StableDiffusionPipeline.from_ckpt("models/revAnimated_v122.safetensors", torch_dtype=torch.float16)
+            self.pipeI2I = StableDiffusionImg2ImgPipeline.from_ckpt("models/revAnimated_v122.safetensors", torch_dtype=torch.float16)
+        elif modelT2I == 4:
+            self.pipeT2I = StableDiffusionPipeline.from_ckpt("models/edgeOfRealism_eorV20Fp16BakedVAE.safetensors", torch_dtype=torch.float16)
+            self.pipeI2I = StableDiffusionImg2ImgPipeline.from_ckpt("models/edgeOfRealism_eorV20Fp16BakedVAE.safetensors", torch_dtype=torch.float16)
+            self.pipeT2I.load_textual_inversion("embeddings/FastNegativeEmbedding.pt")
+        else:
+            self.pipeT2I = StableDiffusionPipeline.from_ckpt("models/chilloutmix_NiPrunedFp16Fix.safetensors", torch_dtype=torch.float16)
+            self.pipeI2I = StableDiffusionImg2ImgPipeline.from_ckpt("models/chilloutmix_NiPrunedFp16Fix.safetensors", torch_dtype=torch.float16)
+            self.pipeT2I.load_textual_inversion("embeddings/bad-picture-chill-32v.pt")
+            #self.pipeI2I.load_textual_inversion("embeddings/bad-picture-chill-32v.pt")
 
-dif_prompts = [
-"small halfling man, merchant, middle-aged, older, some wrinkles, wide devious grin, slicked-back black hair, medieval, mischievous, colorful clothes, backpack with many things, goatee, stubble, inviting gesture, next to donkey, donkey in front of cart, full body"
-]
+        self.pipeT2I.scheduler = DPMSolverMultistepScheduler.from_config(self.pipeT2I.scheduler.config)
+        # self.pipeT2I.feature_extractor = CLIPImageProcessor()# .from_config(self.pipeT2I.feature_extractor.config)
+        # self.pipeT2I.tokenizer = CLIPTokenizer()#.from_config(self.pipeT2I.tokenizer.config)
 
-dif_prompt_styles = [
-"(masterpiece), High detail color photo, a professional photo, (realistic, photorealism:1. 5), (highest quality), (best shadow), ultra-high resolution, physics-based rendering, photo, realism, high contrast, 8k HD high definition detailed realistic, detailed, best quality, Nikon d850 film, cinestill 800",
-"character sheet, concept design, contrast, style by kim jung gi, zabrocki, karlkka, jayison devadas, trending on artstation, 8k, ultra wide angle, pincushion lens effect",
-"epic concept art by barlowe wayne, ruan jia and greg rutkowski, light effect, volumetric light, 3d, ultra clear detailed. octane render. 8k. dark green, light grey blue and golden colour scheme",
-"award winning watercolor pen illustration, detailed, disney, isometric illustration, drawing, by Stephen Hillenburg, Matt Groening, Albert Uderzo",
-"23rd century scientific schematics, blueprint, hyperdetailed vector technical documents, callouts, archviz, legend, patent registry",
-"steampunk cybernetic biomechanical, 3d model, very coherent symmetrical artwork, unreal engine realistic render, 8 k, micro detail, intricate, elegant, highly detailed, centered, digital painting, artstation, smooth, sharp focus, illustration, artgerm, tomasz alen kopera, wlop",
-"epic, by Andrew McCarthy, Navaneeth Unnikrishnan, Manuel Dietrich, photo realistic, 8 k, cinematic lighting, hd, atmospheric, hyperdetailed, trending on artstation, deviantart, photography, glow effect",
-"photograph, photorealistic, vivid, sharp focus, reflection, refraction, sunrays, very detailed, intricate, intense cinematic composition"
-]
+        #self.pipeT2I.enable_model_cpu_offload()
+        self.pipeT2I.enable_attention_slicing()
+        self.pipeT2I.enable_vae_slicing()
+        self.pipeT2I.enable_vae_tiling()
+        self.pipeT2I.safety_checker = dummy
+        self.pipeT2I.enable_xformers_memory_efficient_attention()
+        self.pipeT2I = self.pipeT2I.to("cuda")
 
-dif_neg_prompts = [
-    "(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck",
-    "out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
-    "bad anatomy, bad composition, ugly, abnormal, unrealistic, double, contorted, disfigured, malformed, amateur, extra, duplicate",
-    "",
-]
 
-# guidance_scale: adherence to prompt ~7-8.5 generally good
-# num_inference_steps: ~quality, 50 is normal, less is faster
+        self.pipeI2I.scheduler = DPMSolverMultistepScheduler.from_config(self.pipeI2I.scheduler.config)
 
-#pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=torch.float16)
-pipe = StableDiffusionPipeline.from_ckpt("models/dreamshaper_6BakedVae.safetensors", torch_dtype=torch.float16)
-#pipe = StableDiffusionPipeline.from_ckpt("models/revAnimated_v122.safetensors", torch_dtype=torch.float16)
-#pipe = StableDiffusionPipeline.from_ckpt("models/chilloutmix_NiPrunedFp32Fix.safetensors", torch_dtype=torch.float16)
+        self.pipeI2I.enable_attention_slicing()
+        self.pipeI2I.safety_checker = dummy
+        self.pipeI2I.enable_xformers_memory_efficient_attention()
+        self.pipeI2I = self.pipeI2I.to("cuda")
 
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-pipe.enable_attention_slicing()
-pipe.safety_checker = dummy
-pipe = pipe.to("cuda")
+        #self.pipeUpscale = StableDiffusionUpscalePipeline.from_pretrained(**self.pipeT2I.components, torch_dtype=torch.float16)
+        '''self.pipeUpscale.scheduler = DPMSolverMultistepScheduler.from_config(self.pipeUpscale.scheduler.config)
+        self.pipeUpscale.enable_attention_slicing()
+        self.pipeUpscale.safety_checker = dummy
+        self.pipeUpscale.enable_xformers_memory_efficient_attention()
+        self.pipeUpscale.enable_model_cpu_offload()'''
+        #self.pipeUpscale = self.pipeUpscale.to("cuda")
 
-num_images = 12
-prompt_n = 1
-style_n = 1
-neg_prompt_n = 1
-for prompt_str in dif_prompts:
-    for style_str in dif_prompt_styles:
-        for neg_prompt_str in dif_neg_prompts:
-            prompt = [prompt_str + ", " + style_str] * num_images
-            neg_prompt = [neg_prompt_str] * num_images
+        self.compel = Compel(tokenizer=self.pipeT2I.tokenizer, text_encoder=self.pipeT2I.text_encoder, truncate_long_prompts=True)
 
-            generator = torch.Generator("cuda").manual_seed(100)
-            images = pipe(prompt, negative_prompt=neg_prompt, guidance_scale=7.5, num_inference_steps=20, generator=generator).images
-            grid = image_grid(images, rows=3, cols=4)
-            print("#"*150)
-            print(prompt[0])
+    def paint(self, prompts, neg_prompts, rows, cols, image=None, vary_seed_rows=True, vary_seed_cols=True, w=512, h=512, guide_scale=7.5, inf_steps=25, save_as=""):
+        num_images = len(prompts)
 
-            grid.save(f"results/" + prompt_str.split(", ")[0].replace(" ", "_") + "_" + str(style_n) + neg_prompt_str.split(", ")[0].replace(" ", "_") + ".png")
-            neg_prompt_n +=1
-        style_n +=1
-    prompt_n +=1
+        if vary_seed_rows and vary_seed_cols:
+            generator = [torch.Generator(device="cuda").manual_seed(1001 + i) for i in range(0, num_images)]
+        elif vary_seed_rows:
+            generator = [torch.Generator(device="cuda").manual_seed(1001 + math.floor(i/cols)) for i in range(num_images)]
+        elif vary_seed_cols:
+            generator = [torch.Generator(device="cuda").manual_seed(1001 + (i%cols)) for i in range(num_images)]
+        else:
+            generator = [torch.Generator(device="cuda").manual_seed(1001) for i in range(num_images)]
+
+        prompts_emb, neg_prompts_emb = self.prepare_embeds(prompts, neg_prompts)
+
+        with torch.inference_mode():
+            if image==None:
+                images = self.pipeT2I(prompt_embeds=prompts_emb, negative_prompt_embeds=neg_prompts_emb, width=w, height=h, guidance_scale=guide_scale, num_inference_steps=inf_steps, generator=generator).images
+            else:
+                images = self.pipeI2I(prompt_embeds=prompts_emb, negative_prompt_embeds=neg_prompts_emb, image=image, strength=0.35, guidance_scale=guide_scale, generator=generator).images
+
+        if save_as == "":
+            save_as = prompts[0].split(",")[0].replace(" ", "_") + "_" + neg_prompts[0].split(",")[0].replace(" ", "_")
+        grid = image_grid(images, rows=rows, cols=cols)
+        grid.save(f"results/" + save_as + ".png")
+
+    def paint_axis_from_file(self, base, neg_prompt, fileX, fileY, image=None, modifier_file=""):
+        base_prompts = []
+        neg_prompts = []
+        x_axis = []
+        y_axis = []
+
+        with open("prompts/" + base, "r") as base_file:
+            lines = base_file.readlines()
+            random.shuffle(lines)
+            for line in lines:
+                base_prompts.append(line.replace("\n", ""))
+
+        with open("prompts/" + neg_prompt, "r") as base_file:
+            lines = base_file.readlines()
+            for line in lines:
+                neg_prompts.append(line.replace("\n", ""))
+
+        if fileX != None:
+            if isinstance(fileX, int):
+                x_axis = [""]*fileX
+                vary_seed_cols = True
+            else:
+                vary_seed_cols = False
+                with open("prompts/" + fileX, "r") as X_file:
+                    lines = X_file.readlines()
+                    for line in lines:
+                        x_axis.append(line.replace("\n", ""))
+        else:
+            x_axis.append("")
+
+        if fileY != None:
+            if isinstance(fileY, int):
+                y_axis = [""]*fileY
+                vary_seed_rows = True
+            else:
+                vary_seed_rows = False
+                with open("prompts/" + fileY, "r") as Y_file:
+                    lines = Y_file.readlines()
+                    for line in lines:
+                        y_axis.append(line.replace("\n", ""))
+        else:
+            y_axis.append("")
+
+        if modifier_file != "":
+            with open("prompts/" + modifier_file, "r") as M_file:
+                lines = M_file.readlines()
+                for line in lines:
+                    mod_str = line.replace("\n", "")
+
+
+        cols = len(x_axis)
+        rows = len(y_axis)
+        n_max = len(base_prompts)
+        n = 1
+        for prompt in base_prompts:
+            print(str(n) + "/" + str(n_max) + ": " + prompt)
+            fin_prompts, fin_neg_prompts = self.prompt_pipeline_axis(prompt, neg_prompts, x_axis, y_axis, modifier=mod_str)
+            self.paint(fin_prompts, fin_neg_prompts, rows, cols, image=image, vary_seed_cols=vary_seed_cols, vary_seed_rows=vary_seed_rows)
+            n = n+1
+
+    def prompt_pipeline_axis(self, base_prompt, neg_prompts, x_axis, y_axis, modifier=""):
+        prompts = self.construct_prompt_strings_axis(base_prompt, x_axis, y_axis, modifier=modifier)
+
+        i = 0
+        while len(neg_prompts) < len(prompts):
+            neg_prompts.append(neg_prompts[i])
+            i+=1
+            i=i%len(neg_prompts)
+
+        return prompts, neg_prompts
+
+    def construct_prompt_strings_axis(self, base_prompt, x_axis, y_axis, modifier=""):
+        prompts = []
+        for y in y_axis:
+            for x in x_axis:
+                prompts.append(self.construct_prompt_string_axis(base_prompt, x, y, modifier=modifier))
+        return prompts
+
+    def construct_prompt_string_axis(self, base_prompt, x_axis, y_axis, modifier=""):
+        if modifier != "":
+            modifier = ", " + modifier
+        return base_prompt + ", " + x_axis + ", " + y_axis + modifier
+
+    def prepare_embeds(self, prompts, negative_prompts):
+        prompts_emb = self.compel(prompts)
+        neg_prompts_emb = self.compel(negative_prompts)
+        return prompts_emb, neg_prompts_emb
+
+'''
+image_path = "https://image.t"
+response = requests.get(image_path)
+init_image = PIL.Image.open(BytesIO(response.content)).convert("RGB")
+init_image = init_image.resize((344, 512))
+'''
+
+painter = Painter(4)
+painter.paint_axis_from_file("Prompts", "Negative", 3, 3, modifier_file="HQ")
 
